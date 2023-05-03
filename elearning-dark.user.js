d7e4ebbe3abb31301e0c9907a5d5ec8966fc6b64
Andreas Yozef <andreasyny@gmx.sg>
2023-05-04 06:28:06 +0700

    make the css smaller
    make the duplicates into 1

--- elearning-dark.user.js
+++ elearning-dark.user.js
@@ -43,133 +43,38 @@ addCss(`
 --dark: #11111b !important;
 }
 
-body{
+body, .breadcrumb a{
 	color: #cdd6f4 !important;
 }
-.breadcrumb a{
-	color: #cdd6f4 !important;
-}
-.main-menu li > a{
+#main-header, #subheader, .main-menu li > a{
 	background-color: #1e1e2e !important;
 	color: #cdd6f4 !important;
-}
-#site-menu{
-	background-color: #313244 !important;
-}
-
-#page-header{
-	background-color: #313244 !important;
-}
-
-#main-header{
-    background-color: #1e1e2e !important;
-}
-#subheader{
-    background-color: #1e1e2e !important;
 	background-image: none !important;
 }
+
 .header-tools .theme-plugins .popover-region .nav-link{
 	background-color: #585b70 !important;
 }
 
-#main-content, #region-main{
-	background-color:#313244 !important;
-}
-
-.card-body{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-.list-group > .list-group-item{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-div.p-1:nth-child(2){
-	background-color:#313244 !important;
-}
-
-.bg-white{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-.bg-light{
+#main-content, #region-main, .sliding-panel, #bottom-info > .bar, .section-navigation, #footer , #bottom-info, .card-footer, .box-contacts, .boxlist, .course-panel-content, .student-panel-b, student-panel-a, .card-header, .card-text, .list-group, .message, .bg-light, .bg-white, div.p-1:nth-child(2), .list-group > .list-group-item, .card-body{
 	background-color:#313244 !important;
 	color: #cdd6f4 !important;
 }
 
-.message{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-.list-group{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-.card-text{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
 .card-header{
 	background-color:#313244 !important;
 	color: #cdd6f4 !important;
 	border: solid 1px #fff;
 }
-.student-panel-b, student-panel-a{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-.course-panel-content{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
+
 .modal-header{
 	background-color:#181825 !important;
 	color: #cdd6f4 !important;
 	background-image: none !important;
 }
-.box-contacts, .boxlist{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-.card-title{
-	color: #cdd6f4 !important;
-}
 
-.card-footer{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-
-.dropdown-item{
+.dropdown-menu, .dropdown-item, #page-header, #site-menu{
 	background-color: #313244 !important;
-	color: #cdd6f4 !important;
-}
-
-.dropdown-menu{
-	background-color: #313244 !important;
-	color: #cdd6f4 !important;
-}
-
-a{
-	color: #cdd6f4 !important;
-}
-
-#footer , #bottom-info{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-
-.section-navigation{
-	background-color:#313244 !important;
-}
-
-#bottom-info > .bar{
-	background-color:#313244 !important;
-	color: #cdd6f4 !important;
-}
-
-.panel-link{
-	background-color: #45475a !important;
 	color: #cdd6f4 !important;
 }
 
@@ -178,30 +83,10 @@ a{
 	border-top: solid 1px #45475a !important;
 }
 
-.activity{
-	background-color: #585b70 !important;
-	border-radius: 5px;
-	margin: 50px;
-}
-.contentafterlink{
-	color: #cdd6f4 !important;
-}
-
-.nav{
+.activity-navigation, .nav, .panel-link{
 	background-color: #45475a !important;
 	color: #cdd6f4 !important;
 	border-radius: 5px;
-}
-.activity-navigation{
-	background-color: #45475a !important;
-	color: #cdd6f4 !important;
-	border-radius: 5px;
-}
-
-
-.nav > .nav-item .active{
-	background-color: #585b70 !important;
-	color: #fff !important;
 }
 
 .moodle-tabs{
@@ -211,46 +96,26 @@ a{
 #jump-to-activity{
 	background-color: #6c7086 !important;
 	color: #cdd6f4 !important;
-	border: nonde !important;
+	border: none !important;
 }
 .singleselect{
 	background-color: #6c7086 !important;
 	color: #cdd6f4 !important;
-	border: nonde !important;
-}
-
-.summary{
-	background-color: #585b70 !important;
-	color: #181825 !important;
-	border-radius: 5px;
-}
-
-.sliding-panel{
-	background-color:#313244 !important;
-}
-
-.forumpost{
-	background-color: #585b70 !important;
-	color: #181825 !important;
-	border-radius: 5px;
-}
-
-.post-content-container{
-	background-color: #585b70 !important;
-	color: #cdd6f4 !important;
 	border: none !important;
 }
 
-.message-app{
-	background-color: #313244 !important;
-	border-left: solid 1px #fff;
+.activity, .nav > .nav-item .active, .forumpost, .post-content-container, .summary{
+	background-color: #585b70 !important;
+	color: #181825 !important;
+	border-radius: 5px;
 }
-.drawer-top{
+
+.drawer-top, .message-app{
 	background-color: #313244 !important;
 	border-left: solid 1px #fff;
 }
 
-h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6{
+h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6, a, .card-title, .contentafterlink{
 	color: #cdd6f4 !important;
 }
 
